ec2760007c4e6f7074668b4210a9f73462731d12
Jan Umprecht <umprejan@fit.cvut.cz>
2022-01-31 19:14:05 +0100

    Add verbal result after each round and final results

--- script.js
+++ script.js
@@ -7,11 +7,41 @@ class Counter {
     constructor(playerScore = 0, botScore = 0) {
         this.playerScore = playerScore;
         this.botScore = botScore;
+        this.lastWinner = "";
+        this.lastText = "";
     }
 
     adjust(roundWinner) {
         if (roundWinner == PLAYER) this.playerScore++;
         else if (roundWinner == BOT) this.botScore++;
+    }
+
+    decideRound(playerSelection, computerSelection) {
+        if (playerSelection == computerSelection) {
+            this.lastWinner = TIE;
+            this.lastText = `It's a tie! You both chose ${playerSelection}.`;
+        }
+        else if (playerSelection == "rock" && computerSelection == "scissors" ||
+            playerSelection == "paper" && computerSelection == "rock" ||
+            playerSelection == "scissors" && computerSelection == "paper") {
+            this.lastWinner = PLAYER;
+            this.lastText = `You win, ${playerSelection} beats ${computerSelection}!`;
+        }
+        else {
+            this.lastWinner = BOT;
+            this.lastText = `You lose, ${playerSelection} is beaten by ${computerSelection}!`;
+        }
+        return {
+            winner: this.lastWinner,
+            text: this.lastText
+        };
+    }
+
+    restart() {
+        this.playerScore = 0;
+        this.botScore = 0;
+        this.lastWinner = "";
+        this.lastText = "";
     }
 }
 
@@ -30,46 +60,34 @@ function computerPlay() {
     }
 }
 
-function decideRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return {
-            winner: TIE,
-            text: `It's a tie! You both chose ${playerSelection}.`
-        };
-    }
-
-    if (playerSelection == "rock" && computerSelection == "scissors" ||
-        playerSelection == "paper" && computerSelection == "rock" ||
-        playerSelection == "scissors" && computerSelection == "paper") {
-        return {
-            winner: PLAYER,
-            text: `You win! ${playerSelection} beats ${computerSelection}.`
-        };
-    }
-
-    return {
-        winner: BOT,
-        text: `You lose! ${playerSelection} is beaten by ${computerSelection}.`
-    };
-}
-
-function playRound(playerChoice) {
+function playRound(counter, playerChoice) {
     let player_move = playerChoice;
     let computer_move = computerPlay();
-    return decideRound(player_move, computer_move);
+    return counter.decideRound(player_move, computer_move);
 }
 
 function game() {
-    console.log("hii");
-    
     const buttons = document.querySelectorAll("button");
     let result = new Counter();
     let scoreboard = document.querySelector(".score .counter");
+    let roundResult = document.querySelector(".score .round-result");
+    let gameResult = document.querySelector(".score .game-result");
 
     buttons.forEach(button => button.addEventListener('click', (event) => {
-        result.adjust(playRound(button.value).winner);
+        let playerChoice = button.value;
+        result.adjust(playRound(result, playerChoice).winner);
         console.log(result);
-        scoreboard.textContent = `YOU - ${result.playerScore} : ${result.botScore} - PC`;        
+        scoreboard.textContent = `YOU - ${result.playerScore} : ${result.botScore} - PC`;    
+        roundResult.textContent = `${result.lastText}`;
+        
+        if (result.playerScore > 4) {
+            gameResult.textContent = `You were the first to 5! Congratulations!`;
+            result.restart();
+        }
+        if (result.botScore > 4) {
+            gameResult.textContent = `The computer was the first to 5! Try again.`;
+            result.restart();
+        }
         }
     ));
 }
